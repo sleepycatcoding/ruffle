043adf44d3307e64501b9a40b88d4b8fb5d4ea1a
Minigugus <43109623+Minigugus@users.noreply.github.com>
2022-10-21 20:27:45 +0200

    web: Implement XMLSocket web backend

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -85,6 +85,24 @@ struct RuffleInstance {
     log_subscriber: Arc<Layered<WASMLayer, Registry>>,
 }
 
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen]
+    type JsSocket;
+
+    #[wasm_bindgen(method, js_name = "isOnline")]
+    fn is_connected(this: &JsSocket) -> bool;
+
+    #[wasm_bindgen(method)]
+    fn send(this: &JsSocket, buf: Vec<u8>);
+
+    #[wasm_bindgen(method)]
+    fn poll(this: &JsSocket) -> Option<Vec<u8>>;
+
+    #[wasm_bindgen(method)]
+    fn close(this: &JsSocket);
+}
+
 #[wasm_bindgen(raw_module = "./ruffle-player")]
 extern "C" {
     #[wasm_bindgen(extends = EventTarget)]
@@ -114,6 +132,9 @@ extern "C" {
 
     #[wasm_bindgen(catch, method, js_name = "setFullscreen")]
     fn set_fullscreen(this: &JavascriptPlayer, is_full: bool) -> Result<(), JsValue>;
+
+    #[wasm_bindgen(method, js_name = "connectXmlSocket")]
+    fn connect_xml_socket(this: &JavascriptPlayer, host: &str, port: u16) -> Option<Promise>;
 
     #[wasm_bindgen(method, js_name = "setMetadata")]
     fn set_metadata(this: &JavascriptPlayer, metadata: JsValue);
@@ -530,6 +551,7 @@ impl Ruffle {
             tracing::error!("Unable to create audio backend. No audio will be played.");
         }
         builder = builder.with_navigator(navigator::WebNavigatorBackend::new(
+            js_player.clone(),
             allow_script_access,
             allow_networking,
             config.upgrade_to_https,
--- web/src/navigator.rs
+++ web/src/navigator.rs
@@ -9,6 +9,8 @@ use ruffle_core::loader::Error;
 use ruffle_core::socket::XmlSocketConnection;
 use std::borrow::Cow;
 use std::sync::Arc;
+use std::cell::RefCell;
+use std::rc::Rc;
 use tracing_subscriber::layer::Layered;
 use tracing_subscriber::Registry;
 use tracing_wasm::WASMLayer;
@@ -20,8 +22,11 @@ use web_sys::{
     RequestInit, Response as WebResponse,
 };
 
+use crate::{JavascriptPlayer, JsSocket};
+
 pub struct WebNavigatorBackend {
     log_subscriber: Arc<Layered<WASMLayer, Registry>>,
+    js_player: JavascriptPlayer,
     allow_script_access: bool,
     allow_networking: NetworkingAccessMode,
     upgrade_to_https: bool,
@@ -31,6 +36,7 @@ pub struct WebNavigatorBackend {
 
 impl WebNavigatorBackend {
     pub fn new(
+        js_player: JavascriptPlayer,
         allow_script_access: bool,
         allow_networking: NetworkingAccessMode,
         upgrade_to_https: bool,
@@ -72,6 +78,7 @@ impl WebNavigatorBackend {
         }
 
         Self {
+            js_player,
             allow_script_access,
             allow_networking,
             upgrade_to_https,
@@ -280,9 +287,98 @@ impl NavigatorBackend for WebNavigatorBackend {
 
     fn connect_xml_socket(
         &mut self,
-        _host: &str,
-        _port: u16,
+        host: &str,
+        port: u16,
     ) -> Option<Box<dyn XmlSocketConnection>> {
+        if let Some(promise) = self.js_player.connect_xml_socket(host, port) {
+            let mutex: Box<dyn XmlSocketConnection> = Box::new(PendingConnectionSocket);
+            let mutex = Rc::from(RefCell::from(mutex));
+
+            {
+                let mutex = mutex.clone();
+
+                self.spawn_future(Box::pin(async move {
+                    let socket = JsFuture::from(promise)
+                        .await
+                        .ok()
+                        .and_then(|v| v.dyn_into::<JsSocket>().ok());
+
+                    *mutex.borrow_mut() = if let Some(socket) = socket {
+                        Box::new(socket)
+                    } else {
+                        Box::new(DenySocket)
+                    };
+
+                    Ok(())
+                }));
+            }
+
+            return Some(Box::new(DelegatedSocketConnection(mutex)));
+        }
         None
+    }
+}
+
+struct DelegatedSocketConnection(Rc<RefCell<Box<dyn XmlSocketConnection>>>);
+
+impl XmlSocketConnection for DelegatedSocketConnection {
+    fn is_connected(&self) -> Option<bool> {
+        self.0
+            .borrow()
+            .is_connected()
+    }
+
+    fn send(&mut self, buf: Vec<u8>) {
+        self.0
+            .borrow_mut()
+            .send(buf);
+    }
+
+    fn poll(&mut self) -> Option<Vec<u8>> {
+        self.0
+            .borrow_mut()
+            .poll()
+    }
+}
+
+struct PendingConnectionSocket;
+
+impl XmlSocketConnection for PendingConnectionSocket {
+    fn is_connected(&self) -> Option<bool> {
+        None
+    }
+
+    fn send(&mut self, _buf: Vec<u8>) {}
+
+    fn poll(&mut self) -> Option<Vec<u8>> {
+        None
+    }
+}
+
+struct DenySocket;
+
+impl XmlSocketConnection for DenySocket {
+    fn is_connected(&self) -> Option<bool> {
+        Some(false)
+    }
+
+    fn send(&mut self, _buf: Vec<u8>) {}
+
+    fn poll(&mut self) -> Option<Vec<u8>> {
+        None
+    }
+}
+
+impl XmlSocketConnection for JsSocket {
+    fn is_connected(&self) -> Option<bool> {
+        Some(JsSocket::is_connected(self))
+    }
+
+    fn send(&mut self, buf: Vec<u8>) {
+        JsSocket::send(&*self, buf);
+    }
+
+    fn poll(&mut self) -> Option<Vec<u8>> {
+        JsSocket::poll(&*self)
     }
 }
