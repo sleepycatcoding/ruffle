d3cc67857aacae12bda530bfca7687ac1b9ec410
sleepycatcoding <131554884+sleepycatcoding@users.noreply.github.com>
2023-07-31 21:39:14 +0300

    core: Add SocketKind
    This allows adding new socket types easily

--- core/src/socket.rs
+++ core/src/socket.rs
@@ -1,5 +1,5 @@
 use crate::{
-    avm2::{object::SocketObject, Activation, Avm2, EventObject, TObject},
+    avm2::{object::{SocketObject, XmlSocketObject}, Activation, Avm2, EventObject, TObject},
     backend::navigator::NavigatorBackend,
     context::UpdateContext,
 };
@@ -13,15 +13,22 @@ use std::{
 
 pub type SocketHandle = Index;
 
+#[derive(Clone, Copy, Collect)]
+#[collect(no_drop)]
+pub enum SocketKind<'gc> {
+    Avm2Socket(SocketObject<'gc>),
+    Avm2XmlSocket(XmlSocketObject<'gc>),
+}
+
 #[derive(Collect)]
 #[collect(no_drop)]
 struct Socket<'gc> {
-    target: SocketObject<'gc>,
+    target: SocketKind<'gc>,
     sender: RefCell<Sender<Vec<u8>>>,
 }
 
 impl<'gc> Socket<'gc> {
-    fn new(target: SocketObject<'gc>, sender: Sender<Vec<u8>>) -> Self {
+    fn new(target: SocketKind<'gc>, sender: Sender<Vec<u8>>) -> Self {
         Self {
             target,
             sender: RefCell::new(sender),
@@ -70,7 +77,7 @@ impl<'gc> Sockets<'gc> {
         }
     }
 
-    pub fn connect(
+    pub fn connect_avm2(
         &mut self,
         backend: &mut dyn NavigatorBackend,
         target: SocketObject<'gc>,
@@ -79,7 +86,7 @@ impl<'gc> Sockets<'gc> {
     ) {
         let (sender, receiver) = channel();
 
-        let socket = Socket::new(target, sender);
+        let socket = Socket::new(SocketKind::Avm2Socket(target), sender);
         let handle = self.sockets.insert(socket);
 
         // NOTE: This call will send SocketAction::Connect to sender with connection status.
@@ -133,9 +140,26 @@ impl<'gc> Sockets<'gc> {
                         None => continue,
                     };
 
-                    let connect_evt =
-                        EventObject::bare_default_event(&mut activation.context, "connect");
-                    Avm2::dispatch_event(&mut activation.context, connect_evt, target.into());
+                    match target {
+                        SocketKind::Avm2Socket(target) => {
+                            let connect_evt =
+                                EventObject::bare_default_event(&mut activation.context, "connect");
+                            Avm2::dispatch_event(
+                                &mut activation.context,
+                                connect_evt,
+                                target.into(),
+                            );
+                        }
+                        SocketKind::Avm2XmlSocket(target) => {
+                            let connect_evt =
+                                EventObject::bare_default_event(&mut activation.context, "connect");
+                            Avm2::dispatch_event(
+                                &mut activation.context,
+                                connect_evt,
+                                target.into(),
+                            );
+                        }
+                    }
                 }
                 SocketAction::Connect(
                     handle,
@@ -147,23 +171,32 @@ impl<'gc> Sockets<'gc> {
                         None => continue,
                     };
 
-                    let io_error_evt = activation
-                        .avm2()
-                        .classes()
-                        .ioerrorevent
-                        .construct(
-                            &mut activation,
-                            &[
-                                "ioError".into(),
-                                false.into(),
-                                false.into(),
-                                "Error #2031: Socket Error.".into(),
-                                2031.into(),
-                            ],
-                        )
-                        .expect("IOErrorEvent should be constructed");
+                    match target {
+                        SocketKind::Avm2Socket(target) => {
+                            let io_error_evt = activation
+                                .avm2()
+                                .classes()
+                                .ioerrorevent
+                                .construct(
+                                    &mut activation,
+                                    &[
+                                        "ioError".into(),
+                                        false.into(),
+                                        false.into(),
+                                        "Error #2031: Socket Error.".into(),
+                                        2031.into(),
+                                    ],
+                                )
+                                .expect("IOErrorEvent should be constructed");
 
-                    Avm2::dispatch_event(&mut activation.context, io_error_evt, target.into());
+                            Avm2::dispatch_event(
+                                &mut activation.context,
+                                io_error_evt,
+                                target.into(),
+                            );
+                        }
+                        SocketKind::Avm2XmlSocket(_target) => todo!(),
+                    }
                 }
                 SocketAction::Data(handle, data) => {
                     let target = match activation.context.sockets.sockets.get(handle) {
@@ -172,27 +205,36 @@ impl<'gc> Sockets<'gc> {
                         None => continue,
                     };
 
-                    let bytes_loaded = data.len();
-                    target.read_buffer().extend(data);
+                    match target {
+                        SocketKind::Avm2Socket(target) => {
+                            let bytes_loaded = data.len();
+                            target.read_buffer().extend(data);
 
-                    let progress_evt = activation
-                        .avm2()
-                        .classes()
-                        .progressevent
-                        .construct(
-                            &mut activation,
-                            &[
-                                "socketData".into(),
-                                false.into(),
-                                false.into(),
-                                bytes_loaded.into(),
-                                //NOTE: bytesTotal is not used by socketData event.
-                                0.into(),
-                            ],
-                        )
-                        .expect("ProgressEvent should be constructed");
+                            let progress_evt = activation
+                                .avm2()
+                                .classes()
+                                .progressevent
+                                .construct(
+                                    &mut activation,
+                                    &[
+                                        "socketData".into(),
+                                        false.into(),
+                                        false.into(),
+                                        bytes_loaded.into(),
+                                        //NOTE: bytesTotal is not used by socketData event.
+                                        0.into(),
+                                    ],
+                                )
+                                .expect("ProgressEvent should be constructed");
 
-                    Avm2::dispatch_event(&mut activation.context, progress_evt, target.into());
+                            Avm2::dispatch_event(
+                                &mut activation.context,
+                                progress_evt,
+                                target.into(),
+                            );
+                        }
+                        SocketKind::Avm2XmlSocket(_target) => todo!(),
+                    }
                 }
                 SocketAction::Close(handle) => {
                     let target = match activation.context.sockets.sockets.get(handle) {
@@ -201,9 +243,18 @@ impl<'gc> Sockets<'gc> {
                         None => continue,
                     };
 
-                    let close_evt =
-                        EventObject::bare_default_event(&mut activation.context, "close");
-                    Avm2::dispatch_event(&mut activation.context, close_evt, target.into());
+                    match target {
+                        SocketKind::Avm2Socket(target) => {
+                            let close_evt =
+                                EventObject::bare_default_event(&mut activation.context, "close");
+                            Avm2::dispatch_event(&mut activation.context, close_evt, target.into());
+                        }
+                        SocketKind::Avm2XmlSocket(target) => {
+                            let close_evt =
+                                EventObject::bare_default_event(&mut activation.context, "close");
+                            Avm2::dispatch_event(&mut activation.context, close_evt, target.into());
+                        }
+                    }
                 }
             }
         }
