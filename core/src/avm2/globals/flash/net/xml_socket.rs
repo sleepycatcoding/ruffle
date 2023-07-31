1fe53c6d109594edaa514f28ee354a77753d0e78
sleepycatcoding <131554884+sleepycatcoding@users.noreply.github.com>
2023-07-31 20:53:51 +0300

    avm2: Implement connected()

--- core/src/avm2/globals/flash/net/xml_socket.rs
+++ core/src/avm2/globals/flash/net/xml_socket.rs
@@ -1,15 +1,27 @@
 pub use crate::avm2::object::xml_socket_allocator;
 use crate::avm2::parameters::ParametersExt;
 use crate::avm2::{Activation, Error, Object, TObject, Value};
-use crate::{avm2_stub_getter, avm2_stub_method};
+use crate::avm2_stub_method;
+use crate::context::UpdateContext;
 
 pub fn get_connected<'gc>(
     activation: &mut Activation<'_, 'gc>,
-    _this: Object<'gc>,
+    this: Object<'gc>,
     _args: &[Value<'gc>],
 ) -> Result<Value<'gc>, Error<'gc>> {
-    avm2_stub_getter!(activation, "flash.net.XMLSocket", "connected");
-    Ok(Value::Bool(false))
+    let xml_socket = match this.as_xml_socket() {
+        Some(xml_socket) => xml_socket,
+        None => return Ok(Value::Undefined),
+    };
+
+    let UpdateContext { sockets, .. } = &mut activation.context;
+
+    let handle = match xml_socket.handle() {
+        Some(handle) => handle,
+        None => return Ok(Value::Bool(false)),
+    };
+
+    Ok(Value::Bool(sockets.is_connected(handle)))
 }
 
 pub fn get_timeout<'gc>(
